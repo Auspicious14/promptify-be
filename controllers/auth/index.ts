915cc30876dd564feb4276bd79d7dc0b96f85136
Uthman Abdulganiyu <uthmanabdulganiyu2019@gmail.com>
2025-05-09 16:25:39 +0100

    change capital letter N

--- controllers/auth/index.ts
+++ controllers/auth/index.ts
@@ -45,7 +45,7 @@ export const login = async (req: Request, res: Response) => {
     res.cookie("token", token, {
       httpOnly: true,
       secure: process.env.NODE_ENV === "production",
-      sameSite: "none",
+      sameSite: "None" as "none",
       domain:
         process.env.NODE_ENV === "production"
           ? "promptify-be.vercel.app"
