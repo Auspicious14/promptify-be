0c2d9227899e3562115f4359fea05d04d0015ce1
Uthman Abdulganiyu <uthmanabdulganiyu2019@gmail.com>
2025-05-09 16:55:17 +0100

    [refactor] change sameSite to lax

--- controllers/auth/index.ts
+++ controllers/auth/index.ts
@@ -45,7 +45,7 @@ export const login = async (req: Request, res: Response) => {
     res.cookie("token", token, {
       httpOnly: true,
       secure: process.env.NODE_ENV === "production",
-      sameSite: "None" as "none",
+      sameSite: "lax",
       domain:
         process.env.NODE_ENV === "production"
           ? "promptify-be.vercel.app"
